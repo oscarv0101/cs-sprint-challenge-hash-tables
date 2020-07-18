d2b7a4c4b058cee1b13e50184bc3caca31bfdf13
Oscar Valenzuela <oscar.valenzuela69@gmail.com>
2020-07-17 20:39:39 -0700

    completed ex2.py

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,25 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # hash table to store route's airport source and destination info
+    cache = {}
+    # initialize route array, next we need to add the original source airport
+    route = [None] * length
 
+    # iterate thru each ticket and store its destination in the ht
+    for t in tickets:
+        cache[t.source] = t.destination
+
+    # first ticket along the route has NONE source, so we start there
+    dest = cache["NONE"]
+
+    # next we find each ticket's next destination and add it to the destinations array
+    # flight is current ticket
+    for flight in range(length):
+        # record value of next destination
+        route[flight] = dest
+        # the next destination is the value in the ht, the key is the destination just added to the array 
+        # aka the next flight in the trip
+        # continue until complete
+        dest = cache[dest]
     return route
